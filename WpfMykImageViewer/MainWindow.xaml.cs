0785c3403aa6020740235ffa9ee771061f554b3d
miyukic <m.yukic10@gmail.com>
2022-07-30 09:41:45 +0900

    update: メニューの幅とか、イメージのドロップとか

--- WpfMykImageViewer/MainWindow.xaml.cs
+++ WpfMykImageViewer/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,16 +23,44 @@ namespace WpfMykImageViewer {
     /// </summary>
     public partial class MainWindow : Window {
 
-        public MainWindow() => InitializeComponent();
+        private string? imageFilePath = null;
+        private System.Windows.Media.Imaging.BitmapImage bitmapImage = new();
 
+        public MainWindow() => InitializeComponent();
 
         private void OpenButton(object sender, RoutedEventArgs e) {
             //MessageBox.Show("OpenButton");
             var dialog = new CommonOpenFileDialog();
             var combox = new CommonFileDialogComboBox();
             dialog.Controls.Add(combox);
-            dialog.ShowDialog();
+            //dialog.fi = "Image File(*.bmp,*.jpg,*.png,*.tif)|*.bmp;*.jpg;*.png;*.tif|Bitmap(*.bmp)|*.bmp|Jpeg(*.jpg)|*.jpg|PNG(*.png)|*.png";
+            CommonFileDialogResult result = dialog.ShowDialog();
+            imageFilePath = dialog.FileName;
+            Console.WriteLine(imageFilePath);
+            try {
+                using (var fs = new FileStream(imageFilePath, FileMode.Open)) {
+                    bitmapImage.BeginInit();
+                    bitmapImage.StreamSource = fs;
+                    //bitmapImage.DecodePixelWidth = 500;
+                    //画像をデコードすることでメモリ消費量を抑えられる。
+                    bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmapImage.CreateOptions = BitmapCreateOptions.None;
+                    bitmapImage.EndInit();
+                    bitmapImage.Freeze();
+                }
+            } catch(Exception ex) {
+                System.Console.Error.WriteLine(ex.StackTrace);
+                MessageBox.Show("画像ファイルを選んでください");
+            }
+
+
+            image.Source = bitmapImage;
         }
+        private void image_Drop(object sender, DragEventArgs e) {
+            var fileName = e.Data.GetData(DataFormats.FileDrop) as string[];
+            if (fileName == null) return;
+        }
+
     }
 
     public class UnitTest { 
