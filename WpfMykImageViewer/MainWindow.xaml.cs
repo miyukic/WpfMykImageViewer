ef77138d26fd599d28134c66da00856f942c4d6d
miyukic <m.yukic10@gmail.com>
2022-08-04 10:18:52 +0900

    update

--- WpfMykImageViewer/MainWindow.xaml.cs
+++ WpfMykImageViewer/MainWindow.xaml.cs
@@ -24,7 +24,8 @@ namespace WpfMykImageViewer {
     public partial class MainWindow : Window {
 
         private string? imageFilePath = null;
-        private Window1? versionWindow = null;
+        private VersionWindow? versionWindow = null;
+        private SettingsWindow? settingsWindow = null;
 
         public MainWindow() => InitializeComponent();
 
@@ -47,6 +48,7 @@ namespace WpfMykImageViewer {
             }
 
             image.Source = bitmapImage;
+            image.Width = bitmapImage.PixelWidth;
         }
 
         private void OpenButton(object sender, RoutedEventArgs e) {
@@ -71,7 +73,7 @@ namespace WpfMykImageViewer {
 
         }
 
-        //終了メニュー
+        //終了メニュークリックイベントハンドラ
         private void MenuItem_Click_Close(object sender, RoutedEventArgs e) {
             this.Close();
         }
@@ -82,20 +84,19 @@ namespace WpfMykImageViewer {
             SetImage(fileName[0]);
         }
 
-        //バージョン表示
+        //バージョン表示メニュークリックイベントハンドラ
         private void MenuItem_Click(object sender, RoutedEventArgs e) {
-            if (versionWindow == null) {
-                versionWindow = new Window1();
+            if (versionWindow == null || (versionWindow != null && !versionWindow.IsVisible)) {
+                versionWindow = new VersionWindow();
                 versionWindow.Owner = this; //子ウィンドウが裏にならないように
                 versionWindow.Show();
+            } else if (versionWindow != null && versionWindow.IsVisible) {
+                return;
             }
         }
 
         private void Window_Activated(object sender, EventArgs e) {
             Console.WriteLine("MainWindow#Window_Activated");
-            if (versionWindow != null) {
-
-            }
         }
 
         private void Window_Deactivated(object sender, EventArgs e) {
@@ -103,6 +104,23 @@ namespace WpfMykImageViewer {
             if (versionWindow != null) {
                 Console.WriteLine("versionWindow#Topmost= " + versionWindow.Topmost);
             }
+        }
+
+        // マウスホイール
+        private void Window_MouseWheel(object sender, MouseWheelEventArgs e) {
+            Console.WriteLine(e.Delta);
+            if (image == null) return;
+            Console.WriteLine("image.Width = " + image.Width);
+            if (!((image.Width + e.Delta) <= 0)) {
+                image.Width += e.Delta;
+            }
+        }
+
+        //環境設定メニュークリックイベントハンドラ
+        private void MenuItem_Click_Settings(object sender, RoutedEventArgs e) {
+            settingsWindow = new();
+            settingsWindow.Owner = this;
+            settingsWindow.Show();
         }
     }
 
